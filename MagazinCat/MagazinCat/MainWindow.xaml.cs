9d8f7a40f6096a6a2098d4cac477b0e0efc91a8e
busovaaleks <busovaaleks@yandex.ru>
2022-03-23 12:28:11 +0300

    first commit

---
+++ MagazinCat/MagazinCat/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static MagazinCat.Models;
+
+namespace MagazinCat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HttpClient client = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string URI = $"https://localhost:7100/api/Sale";
+            Dictionary<string, string> parameters = new Dictionary<string, string> { { "param1", tbDateStart.Text }, { "param2", tbDateEnd.Text } };
+            FormUrlEncodedContent encodedContent = new FormUrlEncodedContent(parameters);
+            HttpResponseMessage response = await client.PostAsync(URI, encodedContent);
+            response.EnsureSuccessStatusCode();
+            string json = response.Content.ReadAsStringAsync().Result;
+            List<Sale> sales = JsonConvert.DeserializeObject<List<Sale>>(json);
+            lv.ItemsSource = sales;
+
+        }
+    }
+}
---
+++ MagazinCat/MagazinCat/Models.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazinCat
+{
+    internal class Models
+    {
+        public class Client
+        {
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+            public string Patronymic { get; set; }
+        }
+        public class Telephone
+        {
+            public int Articul { get; set; }
+            public string NameTelephone { get; set; }
+            public string Category { get; set; }
+            public decimal Cost { get; set; }
+            public int Count { get; set; }
+            public string Manufacturer { get; set; }
+        }
+        public class Sale
+        {
+            public DateTime DateSale { get; set; }
+            public Client Client { get; set; }
+            public List<Telephone> Telephones { get; set; }
+
+        }
+    }
+}
